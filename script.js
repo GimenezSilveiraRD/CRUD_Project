30805f19617f9fcf83fcb7e6af582569ddb33623
Richard Daniel Gimenez Silveira <rdgs_uy_ch@MacBook-Pro-de-Richard.local>
2023-07-22 13:16:02 -0400

    Se ajusta funcionalidad de la agenda

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const taskForm = document.getElementById('task-form');
     const taskTitleInput = document.getElementById('task-title');
     const taskDescriptionInput = document.getElementById('task-description');
+    const taskDateInput = document.getElementById('task-date'); // Agrega el input para la fecha
+    const taskTimeInput = document.getElementById('task-time'); // Agrega el input para la hora
     const tasksContainer = document.getElementById('tasks-container');
     const editTaskForm = document.getElementById('edit-task-form');
     const editTaskTitleInput = document.getElementById('edit-task-title');
@@ -16,25 +18,33 @@ document.addEventListener("DOMContentLoaded", function() {
         e.preventDefault();
         const title = taskTitleInput.value.trim();
         const description = taskDescriptionInput.value.trim();
+        const date = taskDateInput.value; // Obtiene la fecha del formulario directamente
+        const time = taskTimeInput.value; // Obtiene la hora del formulario directamente
 
         // Elimina la clase de error si se agregó anteriormente
         taskTitleInput.classList.remove('error');
         taskDescriptionInput.classList.remove('error');
 
-        if (title && description) {
+        if (title && description && date && time) {
             const task = {
                 id: Date.now(),
                 title,
-                description
+                description,
+                date, // Usa la fecha obtenida del formulario
+                time, // Usa la hora obtenida del formulario
             };
             saveTask(task);
             taskTitleInput.value = '';
             taskDescriptionInput.value = '';
-            loadTasks();
+            taskDateInput.value = ''; // Limpia el input de la fecha
+            taskTimeInput.value = ''; // Limpia el input de la hora
+            loadTasks(); // Carga las tareas nuevamente para mostrar la nueva tarea
         } else {
             // Agrega clase de error a campos vacíos
             if (!title) taskTitleInput.classList.add('error');
             if (!description) taskDescriptionInput.classList.add('error');
+            if (!date) alert('Debes seleccionar una fecha.');
+            if (!time) alert('Debes seleccionar una hora.');
         }
     });
 
@@ -61,11 +71,21 @@ document.addEventListener("DOMContentLoaded", function() {
     function loadTasks() {
         tasksContainer.innerHTML = '';
         const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+        // Ordenar las tareas por fecha y hora (de más antigua a más reciente)
+        tasks.sort((a, b) => {
+            const dateA = new Date(`${a.date} ${a.time}`);
+            const dateB = new Date(`${b.date} ${b.time}`);
+            return dateA - dateB;
+        });
+
         tasks.forEach(task => {
             const li = document.createElement('li');
             li.innerHTML = `
                 <strong>${task.title}</strong>
                 <p>${task.description}</p>
+                <p><em>Fecha: ${task.date}</em></p>
+                <p><em>Hora: ${task.time}</em></p>
                 <button onclick="deleteTask(${task.id})">Delete</button>
                 <button onclick="showEditForm(${task.id})">Edit</button>
             `;
